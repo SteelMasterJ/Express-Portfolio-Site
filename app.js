16a14558129d9a4262b4105bfbcfa4cb6ca7757c
Joseph Steel <joseph.steel.iv@gmail.com>
2020-05-28 16:57:29 -0400

    Working Templates
    completed all the copy for the templates, updated some of the data and images as well.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { projects });
 });
 
 app.get('/about', (req, res) => {
@@ -16,16 +16,36 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/:id', (req, res) => {
-    const { id } = req.params;
-    res.render('project', {
-        title: projects[id].project_name,
-        description: projects[id].description,
-        technologies: projects[id].technologies,
-        liveLink: projects[id].live_link,
-        gitLink: projects[id].github_link,
-        images: projects[id].image_urls
-    });
+    // const { id } = req.params;
+    const projectId = req.params.id;
+    const project = projects.find( ({ id }) => id === +projectId );
+
+    if (project) {
+        res.render('project', {
+            title: projects[projectId].project_name,
+            description: projects[projectId].description,
+            technologies: projects[projectId].technologies,
+            liveLink: projects[projectId].live_link,
+            gitLink: projects[projectId].github_link,
+            images: projects[projectId].image_urls
+        });
+    } else {
+        res.sendStatus(404);
+    }
 });
+
+
+// app.use(function(req, res, next) {
+//     const err = res.status(404);
+//     next(err);
+//   });
+
+// app.use((err, req, res, next) => {
+//     res.locals.message = err.message;
+//     res.locals.error = err;
+//     res.status(err.status || 500);
+//     res.render('error', err);
+// });
 
 app.listen(3000, () => {
     console.log('the application is running on localhost:3000')
